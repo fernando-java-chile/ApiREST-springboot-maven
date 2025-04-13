964ee01df4a9a057f65e5e988886cddda9fb4018
fernando-java-chile <fernando.java.chile@gmail.com>
2025-04-12 23:41:07 -0400

    first commit

---
+++ src/main/java/InterSystemIrisData/com/example/ApiREST_springboot_maven/ApiRestSpringbootMavenApplication.java
@@ -0,0 +1,13 @@
+package InterSystemIrisData.com.example.ApiREST_springboot_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/InterSystemIrisData/com/example/ApiREST_springboot_maven/ApiRestSpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package InterSystemIrisData.com.example.ApiREST_springboot_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
